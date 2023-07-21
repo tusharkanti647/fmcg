deda897e7321c8f56b40ae33863df8c491923301
Tushar Kanti Das <tusharkanti647@gmail.com>
2023-07-21 22:04:27 +0530

    creat app.js file, and install all dependency

---
+++ app.js
@@ -0,0 +1,11 @@
+const dotenv=require('dotenv');
+const express = require('express');
+const app= express();
+
+dotenv.config({path:"./config.env"})
+const port=process.env.PORT || 8000;
+
+
+app.listen(port, ()=>{
+    console.log(`listening on ${port}`);
+})
