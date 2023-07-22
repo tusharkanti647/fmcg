1cac62257ee221e35256c2232237b21810139720
Tushar Kanti Das <tusharkanti647@gmail.com>
2023-07-22 16:30:03 +0530

    get one product details, only admin delit product edit product. routse add

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -10,7 +10,7 @@ const { productModel } = require("../model/productSchema");
 //----------------------------------------------------------------
 const addProduct = async (req, res) => {
     try {
-        console.log(req.user);
+        //only admins can add products
         if (req.user.isAdmin) {
             const {
                 category,
@@ -63,12 +63,15 @@ const addProduct = async (req, res) => {
 const getProductList = async (req, res) => {
     try {
         let { searchName, page, sortQue, filters } = req.query;
+
+        //default values for search, page, sortQue, filters
         searchName = searchName || "";
         page = page - 1 || 0;
         sortQue = sortQue ? sortQue.split(" ") : ["rating", "-1"];
         filters = filters ? filters.split(" ") : ["electronic"];
         let limit = 8;
 
+        //create sortby object key and sort quary
         let sortBy = {};
         sortBy[sortQue[0]] = parseInt(sortQue[1]);
 
@@ -80,17 +83,19 @@ const getProductList = async (req, res) => {
             .skip(page * limit)
             .limit(limit);
 
+        //data2 check the next page present or not
         const data2 = await productModel.find({ titel: { $regex: searchName, $options: "i" } })
             .where("category")
             .in(filters)
             .sort(sortBy)
             .skip((page + 1) * limit)
             .limit(limit);
+
         let isNextPagePresent = false;
-        if (data2.length>0) {
+        if (data2.length > 0) {
             isNextPagePresent = true;
         }
-        
+
         res.status(200).json({ data: data, isNextPagePresent: isNextPagePresent });
     } catch (e) {
         console.log(e);
@@ -98,4 +103,85 @@ const getProductList = async (req, res) => {
     }
 };
 
-module.exports = {addProduct, getProductList};
+//one product  details get
+//----------------------------------------------------------------------------
+const getOneProductDetails = async (req, res) => {
+    try {
+        const data = await productModel.findById(req.params.id);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(404).send(err);
+    }
+};
+
+//delete product controller
+//----------------------------------------------------------------
+const productDelete = async (req, res) => {
+    try {
+        //only admins can delete products
+        if (req.user.isAdmin) {
+            let _id = req.params.id;
+
+            const data = await productModel.deleteOne({ _id });
+            //console.log(data);
+            res.status(200).json(data);
+        } else {
+            res.status(404).json({ message: "please login first" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({ message: err.message });
+    }
+};
+
+
+//edit product details. only admin can do this.
+//----------------------------------------------------------------
+const editProduct = async (req, res) => {
+    try {
+        if (req.user.isAdmin) {
+            let _id = req.params.id;
+            const {
+                category,
+                imgLink,
+                titel,
+                discountPrice,
+                originalPrice,
+                rating,
+                weight,
+                pcs,
+                description } = req.body;
+
+            //check all filled is filledup or not
+            if (!category || !imgLink || !titel || !discountPrice || !originalPrice || !rating || !weight || !pcs || !description) {
+                res.status(400).json({ message: "please provide data" });
+                return;
+            }
+
+            const data = await productModel.updateOne({ _id: _id }, {
+                $set: {
+                    category,
+                    imgLink,
+                    titel,
+                    discountPrice,
+                    originalPrice,
+                    rating,
+                    about: {
+                        weight,
+                        pcs,
+                        description
+                    }
+                }
+            });
+            res.status(200).json(data);
+        } else {
+            res.status(404).json({ message: "please login first" });
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({ message: err.message });
+    }
+};
+
+module.exports = { addProduct, getProductList, getOneProductDetails, productDelete, editProduct };
--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
-const { addProduct, getProductList } = require('../controllers/productControllers');
+const { addProduct, getProductList, getOneProductDetails, productDelete, editProduct } = require('../controllers/productControllers');
 
 const secretKey = process.env.KEY;
 require("./passport");
@@ -13,6 +13,9 @@ router.use(bodyParser.json());
 //product routers
 router.post("/add-product", passport.authenticate('jwt', { session: false }), addProduct);
 router.get("/products-search",  getProductList);
+router.get("/get-One-Product/:id",  getOneProductDetails);
+router.delete("/product-delete/:id", passport.authenticate('jwt', { session: false }), productDelete);
+router.put("/product-edit/:id", passport.authenticate('jwt', { session: false }), editProduct);
 
 
 module.exports = router;
